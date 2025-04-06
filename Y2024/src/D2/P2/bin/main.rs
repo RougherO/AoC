a589fb0ef2aa9413c9592c2d1c09638d59b7388e
RougherO <chiragchowdhury51@gmail.com>
2025-04-07 01:27:19 +0530

    Day 2 Part 2 linear

--- Y2024/src/D2/P2/bin/main.rs
+++ Y2024/src/D2/P2/bin/main.rs
@@ -1,17 +1,59 @@
 use std::io::stdin;
 
-fn is_valid(list: Vec<i32>) -> bool {
-    let dec = list[0] > list[1];
+fn f(list: &[i32]) -> (Vec<bool>, Vec<bool>, Vec<bool>, Vec<bool>) {
+    let size = list.len();
 
-    for i in 1..list.len() {
+    let (mut prefixinc, mut prefixdec, mut suffixinc, mut suffixdec) = (
+        vec![false; size],
+        vec![false; size],
+        vec![false; size],
+        vec![false; size],
+    );
+
+    (
+        prefixinc[0],
+        prefixdec[0],
+        suffixinc[size - 1],
+        suffixdec[size - 1],
+    ) = (true, true, true, true);
+
+    for i in 1..size {
         let diff = list[i] - list[i - 1];
 
-        if !((diff < 0 && dec || diff > 0 && !dec) && diff.abs() >= 1 && diff.abs() <= 3) {
-            return false;
+        if diff < 1 || diff > 3 {
+            break;
         }
+        prefixinc[i] = true;
     }
 
-    return true;
+    for i in 1..size {
+        let diff = list[i - 1] - list[i];
+
+        if diff < 1 || diff > 3 {
+            break;
+        }
+        prefixdec[i] = true;
+    }
+
+    for i in (0..size - 1).rev() {
+        let diff = list[i + 1] - list[i];
+
+        if diff < 1 || diff > 3 {
+            break;
+        }
+        suffixinc[i] = true;
+    }
+
+    for i in (0..size - 1).rev() {
+        let diff = list[i] - list[i + 1];
+
+        if diff < 1 || diff > 3 {
+            break;
+        }
+        suffixdec[i] = true;
+    }
+
+    return (prefixinc, prefixdec, suffixinc, suffixdec);
 }
 
 fn main() {
@@ -24,19 +66,31 @@ fn main() {
             .map(|n| n.parse::<i32>().unwrap())
             .collect::<Vec<_>>();
 
-        let valid = (0..list.len())
-            .map(|index| {
-                let mut l = list.clone();
-                l.remove(index);
-                is_valid(l)
-            })
-            .reduce(|x, y| {
-                return x || y;
-            })
-            .unwrap();
+        let (prefixinc, prefixdec, suffixinc, suffixdec) = f(&list);
 
-        if valid {
+        let size = list.len();
+
+        if suffixinc[1] || suffixdec[1] || prefixinc[size - 2] || prefixdec[size - 2] {
             result += 1;
+            return;
+        }
+
+        for i in 1..size - 1 {
+            if prefixinc[i - 1] && suffixinc[i + 1] {
+                let diff = list[i + 1] - list[i - 1];
+
+                if diff >= 1 && diff <= 3 {
+                    result += 1;
+                    return;
+                }
+            } else if prefixdec[i - 1] && suffixdec[i + 1] {
+                let diff = list[i - 1] - list[i + 1];
+
+                if diff >= 1 && diff <= 3 {
+                    result += 1;
+                    return;
+                }
+            }
         }
     });
 
